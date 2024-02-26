438581e3283b7e9e40c9b497a9f9da81a98d4fb3
Elli Raynai <elli@multitouch.studio>
2024-02-26 13:09:34 -0500

    again trying to make gulp work

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,21 @@ const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
 
 gulp.task('sass', function() {
-    return gulp.src(['styles/*.scss', 'styles/*.scss.liquid']).pipe(sass().on('error', sass.logError)).pipe(rename(function(path){
-        if (path.basename.endsWith('.scss')) {
-            path.basename = path.basename.replace('.scss', '');
-            path.extname = '.css.liquid';
-        } else {
-            path.extname = '.css';
-        }
-    })).pipe(gulp.dest('assets'));
+    return gulp.src(['styles/*.scss', 'styles/*.scss.liquid'])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(rename(function(path) {
+            // This checks if the file being processed is a .scss.liquid file
+            if (path.extname === '.liquid') {
+                // Assuming you want Shopify to process Liquid in CSS
+                // If you don't have Shopify specific Liquid code, consider outputting as .css
+                path.basename += '.css';
+                // If no Liquid processing is needed, uncomment the next line and comment out `path.basename += '.css';`
+                // path.extname = '.css';
+            } else {
+                path.extname = '.css';
+            }
+        }))
+        .pipe(gulp.dest('assets'));
 });
 
 gulp.task('watch', function() {
